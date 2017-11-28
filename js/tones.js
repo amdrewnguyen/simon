4598e58f54a8128e7080d08a81915a7618b0470e
Andrew Nguyen <drew.nguyen5@gmail.com>
2017-11-28 15:48:42 -0500

    Added button lighting up sync with sound

--- js/tones.js
+++ js/tones.js
@@ -1,35 +1,41 @@
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-const GREEN = {freq: 415.305, class: "green"};
-const RED = {freq: 311.127, class: "red"};
-const YELLOW = {freq: 247.942, class: "yellow"};
-const BLUE = {freq: 207.652, class: "blue"};
+const GREEN = {freq: 415.305, btnSelector: ".green-btn"};
+const RED = {freq: 311.127, btnSelector: ".red-btn"};
+const YELLOW = {freq: 247.942, btnSelector: ".yellow-btn"};
+const BLUE = {freq: 207.652, btnSelector: ".blue-btn"};
 
 
-function playColor(color, dur=0.42) {
+function handlePress(color, dur=0.42) {
+  const button = document.querySelector(color.btnSelector);
+
   // create Oscillator node
   let oscillator = audioCtx.createOscillator();
   let start = audioCtx.currentTime;
   oscillator.type = 'square';
   oscillator.frequency.value = color.freq; // value in hertz
   oscillator.connect(audioCtx.destination);
+
+  button.classList.add("pressed");
+  setTimeout(()=>button.classList.remove("pressed"), 420);
+
   oscillator.start(start);
   oscillator.stop(start + dur);
 }
 
-// playColor(GREEN, 0, 0.42);
-// playColor(RED, 0, 0.42);
-// playColor(RED, 0.47, 0.42);
-// playColor(YELLOW, 0.94, 0.42);
-// playColor(BLUE, 1.41, 0.42);
+// handlePress(GREEN, 0, 0.42);
+// handlePress(RED, 0, 0.42);
+// handlePress(RED, 0.47, 0.42);
+// handlePress(YELLOW, 0.94, 0.42);
+// handlePress(BLUE, 1.41, 0.42);
 
 document.addEventListener("DOMContentLoaded", () => {
   const greenButton = document.querySelector(".green-btn");
   const redButton = document.querySelector(".red-btn");
   const yellowButton = document.querySelector(".yellow-btn");
   const blueButton = document.querySelector(".blue-btn");
-  greenButton.addEventListener("click", (e) => playColor(GREEN));
-  redButton.addEventListener("click", (e) => playColor(RED));
-  yellowButton.addEventListener("click", (e) => playColor(YELLOW));
-  blueButton.addEventListener("click", (e) => playColor(BLUE));
+  greenButton.addEventListener("click", (e) => handlePress(GREEN));
+  redButton.addEventListener("click", (e) => handlePress(RED));
+  yellowButton.addEventListener("click", (e) => handlePress(YELLOW));
+  blueButton.addEventListener("click", (e) => handlePress(BLUE));
 });
